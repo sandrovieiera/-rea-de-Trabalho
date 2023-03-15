ea5abb72a3d920fdfadb45f78c08974921da149b
sandro <sandro.sand172@gmail.com>
2023-03-15 15:54:53 -0300

    meus primeiro commit

---
+++ projeto/scripts.js
@@ -0,0 +1,29 @@
+let nome = "sandro"
+let idade = 35
+let logado = true
+let lista = ["ovo", "farinha", "açuca", "laite"]
+let nomecompleto = { nome: "sandro", idade: 35 }
+
+/* let logado = false ou true*/
+
+console.log( nome) //string
+console.log(typeof idade) //number
+console.log(typeof logado) //boole
+console.log(typeof lista) //object
+console.log(typeof nomecompleto) //object
+
+
+let cidade = "são paulo"
+let estado = "sergipe"
+
+console.log(cidade)
+console.log(estado)
+let  nomedocarro = "ferrari"
+let preco = "19.99"
+console.log(nomedocarro)
+console.log(preco)
+
+let n1 =15
+let n2 = 20
+let soma = n1 + n2 
+console.log(soma)
