6efa98af3604ab7cc08b08020524871179e0d9c7
sandro <sandro.sand172@gmail.com>
2023-03-20 18:53:35 -0300

    projeto iphone

---
+++ iphone/scripts.js
@@ -0,0 +1,8 @@
+function trocacor(cor){
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+function trocaImagem(imagem){
+    let imgIphone = document.querySelector(".iphone")
+    imgIphone.src = imagem
+}
