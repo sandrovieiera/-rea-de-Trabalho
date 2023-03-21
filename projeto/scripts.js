8dfcb4e0986c420af4ededba3290daccddc84ea0
sandro <sandro.sand172@gmail.com>
2023-03-21 19:19:23 -0300

    modificação iphone

--- projeto/scripts.js
+++ projeto/scripts.js
@@ -1,29 +1,13 @@
-let nome = "sandro"
-let idade = 35
-let logado = true
-let lista = ["ovo", "farinha", "açuca", "laite"]
-let nomecompleto = { nome: "sandro", idade: 35 }
+let idade = 14;
+if (idade >= 18){
+    console.log("você é maior de idade.");
+}else {
+    console.log("você é menor de idade.");
+}
+let idades = 20;
 
-/* let logado = false ou true*/
-
-console.log( nome) //string
-console.log(typeof idade) //number
-console.log(typeof logado) //boole
-console.log(typeof lista) //object
-console.log(typeof nomecompleto) //object
+if (idades == 20) {
+    console.log("você tem 20 anos");
+} 
 
 
-let cidade = "são paulo"
-let estado = "sergipe"
-
-console.log(cidade)
-console.log(estado)
-let  nomedocarro = "ferrari"
-let preco = "19.99"
-console.log(nomedocarro)
-console.log(preco)
-
-let n1 =15
-let n2 = 20
-let soma = n1 + n2 
-console.log(soma)
