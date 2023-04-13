ad23287fd44af9ede8385c563208415e8c72395c
sandrovieira <sandro.sand172@gmail.com>
2023-04-13 06:15:15 -0300

    libera

--- iphone/scripts.js
+++
@@ -1,8 +0,0 @@
-function trocacor(cor){
-    let circulo = document.querySelector(".circulo")
-    circulo.style.background = cor
-}
-function trocaImagem(imagem){
-    let imgIphone = document.querySelector(".iphone")
-    imgIphone.src = imagem
-}
